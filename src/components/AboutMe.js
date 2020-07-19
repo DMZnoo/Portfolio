06ac4d8972ddc2ae60b090b5e25bd953a2741b53
Daniel <daniel.jnw.lee@gmail.com>
2020-07-20 09:20:29 +1200

    fixed arrow dir

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,6 +7,40 @@ import "grid-css/grid.min.css";
 import { slideInLeft, slideInRight, } from 'react-animations';
 import {useLocation} from "react-router-dom";
 
+
+
+const hobbiesConfig =
+    [
+        {
+            hobby:"Gym",
+            image:false,
+            icon:"heartbeat icon",
+            content:"I've been into weight lifting for 10+ years. Les Mills member since 2 0 1 8! :)",
+            segmentColor:"red"
+        },
+        {
+            hobby:"Piano",
+            image:false,
+            icon:"music icon",
+            content:"I've always been into music and had recently picked up piano as a hobby :)",
+            segmentColor:"purple"
+        },
+        {
+            hobby:"Swimming",
+            image:true,
+            ImageICON: <img src={require('../resources/swimming.png')} style={{width:"1.5em",paddingRight:"0.3em",height:"1em"}}/>,
+            content:"Swimmer since I was 9. Definitely not afraid of the water :)",
+            segmentColor:"blue"
+        },
+        {
+            hobby:"Electronic Music",
+            image:true,
+            ImageICON:<img src={require('../resources/Electronic_Music.png')} style={{width:"1.5em",paddingRight:"0.3em",height:"1em"}}/>,
+            content:"I find it great for programming. I've also gone to several raves/festivals.",
+            segmentColor:"yellow"
+        }
+    ];
+
 function AboutMe() {
     const SlideInLeft = styled.div`animation: 2s ${keyframes`${slideInLeft}`} infinite `;
     const SlideInLeftOnce = styled.div`animation: 2s ${keyframes`${slideInLeft}`} 1 `;
@@ -45,8 +79,15 @@ function AboutMe() {
                 <SlideInLeftOnce>
                     <LanguageList/>
                 </SlideInLeftOnce>
+                <div>
+                    <h2 className="ui header">Favorite Activities</h2>
+                    <Hobbies config={hobbiesConfig[0]}/>
+                    <Hobbies config={hobbiesConfig[1]}/>
+                    <Hobbies config={hobbiesConfig[2]}/>
+                    <Hobbies config={hobbiesConfig[3]}/>
+                </div>
 
-                <Hobbies/>
+
             </div>
             <HistoryOfMe/>
             <div className="ui center aligned segment">
--- src/components/Hobbies.js
+++ src/components/Hobbies.js
@@ -1,96 +1,46 @@
 import React, {useState} from 'react';
 
-const Hobbies = () => {
-    const [isShown, SetShown] = useState(0);
+const Hobbies = ({config}) => {
+    const [isShown, SetShown] = useState(false);
     const [isToggled,SetToggle] = useState('down');
-    const handleClick=(num,arrow)=>{
-        if(num === 0)
-            SetToggle('down');
+    const handleClick=(bool)=>{
+        if(bool)
+            SetToggle('up');
         else
-            SetToggle("up");
-        SetShown(num);
+            SetToggle("down");
+        SetShown(bool);
     };
     return(
         <div>
-            <h2 className="ui header">Favorite Activities</h2>
-            <div className="ui center red segment"
-                 onMouseEnter={() => handleClick(1)}
-                 onMouseLeave={() => handleClick(0)}
-                 onClick={()=>handleClick(0)}
+            <div className={`ui center ${config.segmentColor} segment`}
+                 onMouseEnter={() => handleClick(true)}
+                 onMouseLeave={() => handleClick(false)}
+                 onClick={()=>handleClick(false)}
             >
-                <h3><i className="heartbeat icon"></i>Gym
-                    {isShown===1 && (
+                <h3>
+                    {!config.image && (
+                        <i className={`${config.icon} icon`}></i>
+                    )
+                    }
+                    {config.image && (
+                        config.ImageICON
+                    )
+                    }
+                    {config.hobby}
+                    {!isShown && (
                         <i className={`angle ${isToggled} icon`}></i>
                     )
                     }
-                    {isShown!==1 && (
-                        <i className={`angle down icon`}></i>
+                    {isShown && (
+                        <i className={`angle ${isToggled} icon`}></i>
                     )
                     }
                 </h3>
-                {isShown===1 && (
+                {isShown && (
+                    <div className="ui center"><p>{config.content}</p></div>
+                )}
+            </div>
 
-                    <div className="ui center"><p>I've been into weight lifting for 10+ years. Les Mills member since 2 0 1 8! :)</p></div>
-                )}
-            </div>
-            <div className="ui center purple segment"
-                 onMouseEnter={() => handleClick(2)}
-                 onMouseLeave={() => handleClick(0)}
-                 onClick={()=>handleClick(0)}
-            >
-                <h3><i className="music icon"></i>Piano
-                    {isShown===2 && (
-                        <i className={`angle ${isToggled} icon`}></i>
-                    )
-                    }
-                    {isShown!==2 && (
-                        <i className={`angle down icon`}></i>
-                    )
-                    }
-                </h3>
-                {isShown===2 && (
-                    <div className="ui center"><p>I've always been into music and had recently picked up piano as a hobby :)</p></div>
-                )}
-            </div>
-            <div className="ui center blue segment"
-                 onMouseEnter={() => handleClick(3)}
-                 onMouseLeave={() => handleClick(0)}
-                 onClick={()=>handleClick(0)}
-            >
-                <h3><img src={require('../resources/swimming.png')} style={{width:"1.5em",paddingRight:"0.3em",height:"1em"}}/>Swimming
-                    {isShown===3 && (
-                        <i className={`angle ${isToggled} icon`}></i>
-                    )
-                    }
-                    {isShown!==3 && (
-                        <i className={`angle down icon`}></i>
-                    )
-                    }
-                </h3>
-                {isShown===3 && (
-                    <div className="ui center"><p>Swimmer since I was 9. Definitely not afraid of the water :)</p></div>
-                )}
-
-            </div>
-            <div className="ui center yellow segment"
-                 onMouseEnter={() => handleClick(4)}
-                 onMouseLeave={() => handleClick(0)}
-                 onClick={()=>handleClick(0)}
-            >
-                <h3><img src={require('../resources/Electronic_Music.png')} style={{width:"1.5em",paddingRight:"0.3em",height:"1em"}}/>Electronic Music
-                    {isShown===4 && (
-                        <i className={`angle ${isToggled} icon`}></i>
-                    )
-                    }
-                    {isShown!==4 && (
-                        <i className={`angle down icon`}></i>
-                    )
-                    }
-                </h3>
-                {isShown===4 && (
-                    <div className="ui center"><p>I find it great for programming. I've also gone to several raves/festivals.</p></div>
-                )}
-            </div>
         </div>
     )
 };
